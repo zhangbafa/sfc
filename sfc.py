5d1c5b47f05d4cf383915d0f455768802d18cd9a
zhang <1269078063@qq.com>
2020-10-13 15:48:23 +0800

    过滤短时间内重复的消息（来自不同微信群）

--- sfc.py
+++ sfc.py
@@ -10,6 +10,8 @@ import sqlite3
 抓取微信群中的拼车信息，同步到数据库'''
 
 #启动机器人
+dbpath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sfc.db")
+conn = sqlite3.connect(dbpath, check_same_thread=False)
 
 bot = Bot(cache_path=True)
 bot.groups(update=True, contact_only=False)
@@ -18,14 +20,19 @@ bot.groups(update=True, contact_only=False)
 # boring_group = bot.groups().search(u'宿州同乡')[0]
 boring_group1 = bot.groups().search(u'老少')[0]
 boring_group2 = bot.groups().search(u'栏杆')[0]
+boring_group3 = bot.groups().search(u'解集往返宿州')[0]
+boring_group4 = bot.groups().search(u'宿州同乡')[0]
 boring_group1.update_group(members_details=True)
 boring_group2.update_group(members_details=True)
+boring_group3.update_group(members_details=True)
+boring_group4.update_group(members_details=True)
+
 
 # self.connection = sqlite3.connect(self.database, timeout=3, isolation_level=None,check_same_thread=False)
 
 
 
-@bot.register([boring_group1,boring_group2],msg_types="Text",except_self=False)
+@bot.register([boring_group1,boring_group2,boring_group3,boring_group4],msg_types="Text",except_self=False)
 #注册消息响应事件，一旦收到铲屎群的消息，就执行下面的代码同步消息。机器人自己在群里发布的信息也进行同步。
 def sync_my_groups(msg):    
     # sync_message_in_groups(msg, my_groups)
@@ -36,25 +43,29 @@ def sync_my_groups(msg):
 			content = msg.text
 			createtime = msg.raw['CreateTime']
 			phone = re.findall("1\d{10}", content)
+			wxmsg = sender + ":" + str(createtime)
+			print(wxmsg)
 			#1为车找人
 			type=0
-			if "车找人" in content:
+			if "找人" in content:
 				type=1
-			dbpath = os.path.join(os.path.abspath(os.path.dirname(__file__)),"sfc.db")
-			conn = sqlite3.connect(dbpath,check_same_thread=False)
+
 			cursor = conn.cursor()
 			
 			selectsql = "select * from posts where sender=? order by createtime desc limit 1 "
 			cursor.execute(selectsql, (sender,))
 			result = cursor.fetchone()
-			if result == None or createtime-result[4]>60:
+
+			if (result == None or (int(createtime)-int(result[4])>300)) and len(content)>11:
 				data = [sender, phone[0],content,createtime,type]
 				sql = "INSERT INTO posts(sender,phone,content,createtime,type) VALUES(?,?,?,?,?)"
 				cursor.execute(sql, data)
 				cursor.close()
 				conn.commit()
-			conn.close()
-			print(sender,content,phone,createtime)			
+			# conn.close()
+
+			print("*"*20)
+			# print(sender,content,phone,createtime)
         
 	except BaseException as e:
 		print(e)
@@ -62,3 +73,5 @@ def sync_my_groups(msg):
 		print('%s[%s] : %s [success]' % (sender, dt, phone[0]))
    
 embed()
+
+
